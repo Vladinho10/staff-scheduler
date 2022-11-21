bfe1e8a6cc3cd8ae36da528757fae870e540c166
Vladinho10 <33626746+Vladinho10@users.noreply.github.com>
2022-11-21 22:54:12 +0400

    Initial commit

---
+++ resource-bodies/controllers.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = `'use strict';
+const { serviceName } = require('dir');
+
+class controllerName {
+    static async getOne(req: Request, res: CustomResponse) {
+        
+    }
+    
+    static async getMany(req: Request, res: CustomResponse) {
+        
+    }
+
+    static async post(req: Request, res: CustomResponse) {
+        
+    }
+
+    static async putOne(req: Request, res: CustomResponse) {
+        
+    }
+
+    static async deleteOne(req: Request, res: CustomResponse) {
+        
+    }
+}
+
+module.exports = {
+    controllerName,
+};
+`;
